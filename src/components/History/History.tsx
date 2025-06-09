7197c238ad31e87dd044d0473e3c5333eb692427
Приказчиков Иван <inprikazchikov@bk.ru>
2025-06-09 19:20:03 +0300

    правка key у history, проверка на наличия сохранений

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -3,13 +3,17 @@ import "./History.scss"
 interface Props {
   history: Number[]
 }
-export const History = ({history}: Props) => {
+export const History = ({ history }: Props) => {
   return (
     <div className="history">
       <ul>
-        {history.map((item) => {
-          return <li key={`${item} ${Math.random}`}>Предыдущий результат: {Number(item)}</li>
-        })}
+        {history ? (
+          history.map((item, index) => {
+            return <li key={index}>Предыдущий результат: {Number(item)}</li>
+          })
+        ) : (
+          <></>
+        )}
       </ul>
     </div>
   )
